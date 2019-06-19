6845f17b7f693bd54903e63fd56f44e7fcbd4a27
Ivan Barreto <ivansbarreto1@gmail.com>
2019-06-19 14:09:26 +1200

    fixing database communication and serving http

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { graphql } = require('graphql');
 const readline = require('readline');
+const graphqlHTTP = require('express-graphql');
+const express = require('express');
+
+const app = express();
 
 const mySchema = require('./schema/main');
 
@@ -13,14 +17,16 @@ const assert = require('assert');
 
 const MONGO_URL = 'mongodb://localhost:27017/test';
 
-MongoClient.connect(MONGO_URL, (err, db) => {
+MongoClient.connect(MONGO_URL, (err, client) => {
   assert.equal(null, err);
+  const db = client.db('test');
   console.log('Connected to MongoDB server');
-
-  rli.question('Client Request: ', inputQuery => {
-    graphql(mySchema, inputQuery, {}, { db }).then(result => {
-      console.log('Server Answer :', result.data);
-      db.close(() => rli.close());
-    });
-  });
+    app.use('/graphql', graphqlHTTP({
+        schema: mySchema,
+        context: {db},
+        graphiql: true
+    }));
+    app.listen(3000, () =>
+        console.log('Running Express.js on port 3000')
+    );
 });
