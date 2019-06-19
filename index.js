a13c44050edd0db4cfeacf4525be5639fe7017a8
Ivan Barreto <ivansbarreto1@gmail.com>
2019-06-19 13:50:05 +1200

    Fixing data fetching from the database

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const {graphql} = require('graphql');
+const { graphql } = require('graphql');
 const readline = require('readline');
 
 const mySchema = require('./schema/main');
+
 const rli = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-rli.question('Client Request: ', inputQuery =>{
-    graphql(mySchema, inputQuery).then(result => {
-        console.log('Server Answer :', result.data);
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
+
+const MONGO_URL = 'mongodb://localhost:27017/test';
+
+MongoClient.connect(MONGO_URL, (err, db) => {
+  assert.equal(null, err);
+  console.log('Connected to MongoDB server');
+
+  rli.question('Client Request: ', inputQuery => {
+    graphql(mySchema, inputQuery, {}, { db }).then(result => {
+      console.log('Server Answer :', result.data);
+      db.close(() => rli.close());
     });
-    rli.close();
+  });
 });
