e21124e00a2270b365350b1b58e0654d693c1aef
Ivan Barreto <ivansbarreto1@gmail.com>
2019-06-20 12:21:01 +1200

    employee queries with string case processing

--- schema/main.js
+++ schema/main.js
@@ -3,19 +3,81 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLList
+    GraphQLList,
+    GraphQLBoolean,
+    GraphQLEnumType
   } = require('graphql');
   
   const roll = () => Math.floor(6 * Math.random()) + 1;
+
+  const toTitleCase = str => {
+    return str.replace(/\w\S*/g, txt =>
+      txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
+  };
+
+  const LetterCaseType = new GraphQLEnumType({
+    name: 'LetterCase',
+    values: {
+      TITLE: {value: 'title'},
+      UPPER: {value: 'upper'},
+      LOWER: {value: 'lower'}
+    }
+  });
+
+  const exampleEmployee = {
+    firstName: 'jane',
+    lastName: 'doe'
+  };
+
+  const EmployeeType = new GraphQLObjectType({
+    name: 'Employee',
+    fields: () => ({
+      name: {
+        type: GraphQLString,
+        args: {
+          upperCase: {type: GraphQLBoolean}
+        },
+        resolve: (object, args) => {
+          let fullName = `${obj.firstName} ${obj.lastName}`; 
+          return args.upperCase ? fullName.toUpperCase() : fullName;
+        }
+      },
+      nameForCase: {
+        type: GraphQLString,
+        args: {
+          letterCase: {type: LetterCaseType}
+        },
+        resolve: (obj, args) => {
+          let fullName = `${obj.firstName} ${obj.lastName}`
+          switch (args.letterCase) {
+            case 'lower':
+              return fullName.toLowerCase();
+            case 'upper':
+              return fullName.toUpperCase();
+            case 'title':
+              return toTitleCase(fullName);
+            default:
+              return fullName;
+          }
+        }
+      },
+      boss: {type: EmployeeType}
+    })
+  });
   
   const queryType = new GraphQLObjectType({
     name: 'RootQuery',
     fields: {
+      exampleEmployee: {
+        type: EmployeeType,
+        resolve: () => exampleEmployee
+      },
       hello: {
         type: GraphQLString,
         resolve: () => 'world'
       },
       diceRoll: {
+        description:  '**Simulate** a dice roll determined by count',
         type: new GraphQLList(GraphQLInt),
         args: {
           count: {
@@ -32,6 +94,7 @@ const {
         }
       },
       usersCount: {
+        description:  'Total number of users in the database',
         type: GraphQLInt,
         resolve: (_, args, { db }) =>
           db.collection('users').count()
@@ -39,6 +102,7 @@ const {
     }
   });
   
+
   const mySchema = new GraphQLSchema({
     query: queryType
   });
